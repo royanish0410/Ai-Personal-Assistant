8a911a244f3780f170e0c9d067aa6342cf5a90e3
royanish123 <anish.anandroy@gmail.com>
2025-07-03 10:36:31 +0530

    Added toggle mode

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
-import { AuthContext } from '@/context/AuthContext';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { AuthContext } from "@/context/AuthContext";
 
 function Provider({
   children,
@@ -18,12 +18,12 @@ function Provider({
       <ConvexProvider client={convex}>
         <AuthContext.Provider value={{ user, setUser }}>
           <NextThemesProvider
-            attribute="class"
+            attribute="class" 
             defaultTheme="light"
-            enableSystem={false}
+            enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <div>{children}</div>
           </NextThemesProvider>
         </AuthContext.Provider>
       </ConvexProvider>
