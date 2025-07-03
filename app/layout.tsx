ef942de114036bacc3d5774eabd4003b10843aa9
royanish123 <anish.anandroy@gmail.com>
2025-07-03 10:39:28 +0530

    some changes made

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,10 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>
-          {children}
-        </Provider>
-        
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
