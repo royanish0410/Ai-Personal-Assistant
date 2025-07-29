c3c7b4fe11bf8b99c7d4c47061336f80fa9a6881
royanish123 <anish.anandroy@gmail.com>
2025-07-29 12:08:30 +0530

    refactor(workspace): restructure workspace layout and logic

--- app/(main)/workspace/page.tsx
+++ app/(main)/workspace/page.tsx
@@ -6,16 +6,16 @@ import ChatUi from './_components/ChatUi'
 function Workspace() {
   return (
         <div className='h-screen fixed w-full'>
-            <div className='grid grid-cols-5'>
-                <div className='hidden md:block'>
+            <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 h-full'>
+                <div className='hidden md:block md:col-span-1'>
                     {/*Assistant List */}
                     <AssistantList />
                 </div>
-                <div className='md:col-span-4 lg:col-span-3'>
+                <div className='col-span-1 sm:col-span-3 md:col-span-4 lg:col-span-3'>
                     {/* Chat ui */}
                     <ChatUi />
                 </div>
-                <div className='hidden lg:block'>
+                <div className='hidden lg:block lg:col-span-1'>
                     {/*Settings */} 
                     <AssistantSettings />
                 </div>
